dc3f9f3935eee5ab2f5f17ff1d395f0602cc75b4
lorrainetechnology <lorrainetechnology@gmail.com>
2022-05-20 23:31:57 +0530

    unit testing moq

---
+++ READMe.md
@@ -0,0 +1,4 @@
+In Package Manager Console, select TestNinja.UnitTests as Default Project, and execute
+install-package NUnit
+Next, install
+install-package NUnit3TestAdapter
---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        Mock<IFileReader> _fileReader;
+        VideoService _videoService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
+        [Test]
+        public void ReadVideoFile_EmptyFile_ReturnErrorMessage()
+        {            
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");            
+            var result = _videoService.ReadVideoTitle();
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
